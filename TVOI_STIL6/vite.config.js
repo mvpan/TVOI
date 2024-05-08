e3e010af01df46eb7c7b200ba8546f5c5b1c7e06
mvpan <mvpan@bk.ru>
2024-05-08 22:26:09 +0300

    Add config

--- TVOI_STIL6/vite.config.js
+++ TVOI_STIL6/vite.config.js
@@ -4,18 +4,5 @@ import Inspect from "vite-plugin-inspect";
 import { resolve } from "path";
 
 export default {
-  plugins: [
-    Inspect({
-      build: true,
-      outputDir: ".vite-inspect",
-    }),
-  ],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, "index.html"), //js файл без type module
-  //       contact: resolve(__dirname, "contact/contacts.html"), //Основная точка входа
-  //     },
-  //   },
-  // },
+  base: "/TVOI/",
 };
